37be20520e0e16adca4c6a79f679fe9b3d8be359
hongsokan_mac <hongsokan@daum.net>
2020-12-09 17:46:13 +0900

    1209 dotenv setting

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
 const express = require('express')
 const app = express()
 const port = 3000
 
+dotenv.config({
+  path: path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV == "production" ? ".env" : ".env.dev"
+  )
+});
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://test:1234@boiler-plate.gy3yl.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${process.env.id}:${process.env.password}@boiler-plate.gy3yl.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
-
-/*
-mongodb+srv://ahs1118:<password>@boiler-plate.gy3yl.mongodb.net/<dbname>?retryWrites=true&w=majority
-*/
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
